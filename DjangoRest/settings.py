f43e17efd5440943db387d51e75b05b7dc1ca064
rohanr <rohanrocket4@gmail.com>
2021-01-28 00:06:52 +0530

    fix: Static files

--- DjangoRest/settings.py
+++ DjangoRest/settings.py
@@ -136,6 +136,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
