39b2e41a0c0a938ccabf098e12818b708b61ec17
rohanr <rohanrocket4@gmail.com>
2021-01-28 00:36:16 +0530

    docs: Heroku configurations

--- DjangoRest/settings.py
+++ DjangoRest/settings.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import environ
 import os
 from pathlib import Path
+import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -138,3 +139,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
